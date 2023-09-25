19671cabc2a9884a6833c59ebd81de1760f7d686
ggpsgeorge <ggpsgeorge@gmail.com>
2023-09-25 09:00:40 -0300

    Spring Initialized

---
+++ src/main/java/com/ggpsgeorge/spring_movie_rest_api/SpringMovieRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ggpsgeorge.spring_movie_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMovieRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMovieRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggpsgeorge/spring_movie_rest_api/SpringMovieRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggpsgeorge.spring_movie_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMovieRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
